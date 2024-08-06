1564ac22327650b7df9371c1bee96824ab4f17fd
annita049 <e.annita2001@gmail.com>
2024-08-07 02:39:02 +0600

    added popup edit option

--- Routers/router.js
+++ Routers/router.js
@@ -2,27 +2,37 @@ const express = require('express');
 const router = express.Router();  // router object is an instance of express.Router()
 const Student = require('../Models/student');
 
-router.get("/home",(req,res)=>{
-    res.render("home");
+router.get('/home',(req, res)=>{
+    res.render('layout',{
+        title: 'Home Page',
+        body: 'pages/home'
+    });
 });  
 // routers.get() adds a new route to the routers object
 
-router.get("/students", async(req,res)=>{
+router.get("/students", async(req, res)=>{
     const students = await Student.find();
-    res.render("student_records",{students: students});
+    res.render('layout',{
+        title: 'Student Records',
+        body: 'pages/all_students',
+        students: students
+    });
 });
 
-router.get("/view/:id", async(req,res)=>{
-    const id = req.params.id;
+router.get("/view/:obj_id", async(req, res)=>{
+    const id = req.params.obj_id;   // unique object id
     const student = await Student.findOne({_id: id});
     res.render("student_view",{student: student});
 });
 
-router.get("/add",(req,res)=>{
-    res.render("add_student");
+router.get("/add",(req, res)=>{
+    res.render('layout',{
+        title: 'Add new Student',
+        body: 'pages/add_student',
+    });
 });
 
-router.post("/add", async(req,res)=>{
+router.post('/add', async(req, res)=>{
     const student = new Student({
         id: req.body.id,
         name: req.body.name,
@@ -37,20 +47,55 @@ router.post("/add", async(req,res)=>{
     // student.save()
     // .then(()=> console.log("data saved successfully"))
     // .catch((err)=> console.log("error saving data!",err)); // blocking call
-    res.redirect("/students");
+    res.redirect('/students');
 });
 
-router.post("/delete", async(req,res)=>{
-    const id = req.body.id;
+router.post('/delete', async(req, res)=>{
+    const obj_id = req.body.obj_id;  // object unique id
     // console.log(id);
     try{
-        await Student.deleteOne({_id: id});
+        await Student.deleteOne({_id: obj_id});
         res.json({success: true, msg: "Student info deleted successfully!"});
     }
     catch(err){
         console.log(err);
-        res.json({success: true, msg: "failed"});
+        res.json({success: true, msg: "failed deleting student data"});
     }
 });
+
+
+router.get("/edit/:id", async(req, res)=>{
+    const _id = req.params.id;   // unique object id
+    const student = await Student.findOne({_id: _id});
+    res.json(student);
+    // res.render('layout',{
+    //     title: 'Student Records',
+    //     body: 'pages/all_students',
+    //     student: student
+    // });
+});
+
+router.post('/edit', async(req, res)=>{
+    const {_id, id, name, cgpa, department} = req.body;
+
+    try {
+
+        // if(!_id) res.status(500).json({success: false})
+        response = await Student.updateOne(
+            {_id: _id},
+            {$set: {id, name, cgpa, department}}
+        );
+
+        // res.status(200).json({success: true});
+        // if(response.matchedCount) res.status(200).json({success: true});
+        // res.send(response);
+        res.redirect('/students');
+    }
+    catch(err){
+        console.log("can't update data!", err);
+        res.status(500).json({success: false});
+    }
+});
+
 
 module.exports = router;
